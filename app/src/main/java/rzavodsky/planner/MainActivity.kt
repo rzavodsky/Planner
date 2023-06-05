a0ac972dd8193a59c8ef5d5a0e3580f10e6c7c67
rzavodsky <zavodsky.richard1@gmail.com>
2023-06-05 12:01:27 +0200

    Project setup

---
+++ app/src/main/java/rzavodsky/planner/MainActivity.kt
@@ -0,0 +1,11 @@
+package rzavodsky.planner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
