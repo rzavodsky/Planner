44d36a9e3a570f4c2dcd055fd3c2c0f56dc00a09
rzavodsky <zavodsky.richard1@gmail.com>
2023-06-11 12:23:12 +0200

    Added functions to convert dp to px

---
+++ app/src/main/java/rzavodsky/planner/DpConversion.kt
@@ -0,0 +1,10 @@
+package rzavodsky.planner
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+val Float.dpToPx: Float
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
+
+val Int.dpToPx: Float
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, toFloat(), Resources.getSystem().displayMetrics)
