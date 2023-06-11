30524ebad61fff3888cc88e9ea224e4b417febdd
rzavodsky <zavodsky.richard1@gmail.com>
2023-06-11 12:27:33 +0200

    Added EditableDayView to MainActivity

--- app/src/main/java/rzavodsky/planner/MainActivity.kt
+++ app/src/main/java/rzavodsky/planner/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dayView = findViewById<EditableDayView>(R.id.day_view)
+        val adapter = PlanBlockAdapter()
+        adapter.data.add(PlanBlock(1, 2, "Test"))
+        adapter.data.add(PlanBlock(4, 2, "Test2"))
+        dayView.editableAdapter = adapter
     }
 }
