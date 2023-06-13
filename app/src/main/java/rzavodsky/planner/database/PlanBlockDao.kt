28fb0ebb49544e936bd1b950923eac0b788f13e2
rzavodsky <zavodsky.richard1@gmail.com>
2023-06-13 22:51:37 +0200

    Added deleting to PlanDetailFragment

--- app/src/main/java/rzavodsky/planner/database/PlanBlockDao.kt
+++ app/src/main/java/rzavodsky/planner/database/PlanBlockDao.kt
@@ -2,6 +2,7 @@ package rzavodsky.planner.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
@@ -14,6 +15,9 @@ interface PlanBlockDao {
 
     @Update
     suspend fun update(block: PlanBlock)
+
+    @Delete
+    suspend fun delete(block: PlanBlock)
 
     @Query("SELECT * FROM plan_blocks WHERE date = :date")
     fun getAllPlansForDay(date: LocalDate): LiveData<List<PlanBlock>>
--- app/src/main/java/rzavodsky/planner/fragments/PlanDetailFragment.kt
+++ app/src/main/java/rzavodsky/planner/fragments/PlanDetailFragment.kt
@@ -3,30 +3,60 @@ package rzavodsky.planner.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.coroutines.launch
+import rzavodsky.planner.R
+import rzavodsky.planner.database.PlanBlock
+import rzavodsky.planner.database.PlanBlockDao
 import rzavodsky.planner.database.PlanBlockDatabase
 import rzavodsky.planner.databinding.FragmentPlanDetailBinding
+import java.time.LocalDate
 
-class PlanDetailFragment : Fragment() {
-    val args: PlanDetailFragmentArgs by navArgs()
+class PlanDetailFragment : Fragment(), MenuProvider {
+    private val args: PlanDetailFragmentArgs by navArgs()
+    private lateinit var binding: FragmentPlanDetailBinding
+    private lateinit var dataSource: PlanBlockDao
+    private lateinit var planBlock: PlanBlock
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentPlanDetailBinding.inflate(inflater)
+        binding = FragmentPlanDetailBinding.inflate(inflater)
+        dataSource = PlanBlockDatabase.getInstance(requireContext()).planBlockDao
 
-        val dataSource = PlanBlockDatabase.getInstance(requireContext()).planBlockDao
 
         lifecycleScope.launch {
-            val plan = dataSource.getPlan(args.planId)
-            binding.planBlock = plan
+            planBlock = dataSource.getPlan(args.planId)
+            binding.planBlock = planBlock
+            requireActivity().addMenuProvider(this@PlanDetailFragment, viewLifecycleOwner)
         }
 
         return binding.root
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        if (planBlock.date == LocalDate.now()) {
+            menuInflater.inflate(R.menu.plan_detail_menu, menu)
+        }
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_delete -> {
+            lifecycleScope.launch {
+                dataSource.delete(binding.planBlock!!)
+                findNavController().navigateUp()
+            }
+            true
+        }
+        else -> false
     }
 }
